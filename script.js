4d03bec6cc9fdaabc0bbd4bf868ec73bcae52549
pedram.zamaninezhad <pzamaninejad@yahoo.com>
2024-05-10 00:35:40 +0330

    finish part 126

--- script.js
+++ script.js
@@ -99,6 +99,8 @@ function goFight() {
     update(locations[3]);
     monsterHealth = monsters[fighting].health;
     monsterStats.style.display = 'block';
+    monsterName.innerText = monsters[fighting].name
+    monsterHealthText.innerText = monsterHealth;
 }
 
 function buyHealth() {
@@ -163,9 +165,27 @@ function fightBeast() {
 }
 
 function attack() {
+    text.innerText = "The " + monsters[fighting].name + " attacks.";
+    text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
+    health -= monsters[fighting].level;
+    monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp + 1);
+    healthText.innerText = health
+    monsterHealthText.innerText = monsterHealth;
+    if (health <= 0) {
+        lose();
+    } else if (monsterHealth <= 0) {
+        defeatMonster();
+    }
+}
+
+function dodge() {
 
 }
 
-function dodge(){
+function defeatMonster() {
+
+}
+
+function lose() {
 
 }
